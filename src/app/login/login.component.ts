1b6428dee56888d0c6ac186858cb5e00b236f3cd
Andrew Rosário <andrew.arosario@gmail.com>
2020-03-25 16:13:44 -0300

    criando formulário de login e cadastro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
+  public loginForm: FormGroup;
+  public isLogin = true;
+
   constructor(
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.criarForm();
+  }
+
+  criarForm(): void {
+    this.loginForm = this.formBuilder.group({
+      usuario: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+  }
+
+  toggleLogin(): void {
+    this.isLogin = !this.isLogin;
+  }
+
+  get usuario() {
+    return this.loginForm.get('usuario');
+  }
+
+  get senha() {
+    return this.loginForm.get('senha');
   }
 
 }
