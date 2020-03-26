905495e0a15822cd385b609f737baa03c8444a24
Andrew Rosário <andrew.arosario@gmail.com>
2020-03-26 15:04:26 -0300

    importando HttpClient

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RequestInterceptor } from './interceptors/request.interceptor';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [
     {
