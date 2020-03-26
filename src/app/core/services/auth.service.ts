d55f1b327a97f22d6d272c8b209aa9b779690e91
Andrew Rosário <andrew.arosario@gmail.com>
2020-03-26 15:26:58 -0300

    implementando métodos do AuthService

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/usuario';
+import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
+
+const API = environment.api;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  cadastrar(usuario: Usuario) {
+    return this.http.post<any>(API + 'usuarios/cadastro', usuario);
+  }
+
+  autenticar(usuario: Usuario) {
+
+    return this.http
+      .post<Usuario>(
+        API + 'usuarios/login',
+        usuario,
+        { observe: 'response'}
+      )
+      .pipe(
+        tap((res: any) => {
+          const authToken = res.body.token;
+          // this.usuarioService.setToken(authToken);
+          console.log(`Usuário ${usuario} autenticado com o token ${authToken}`);
+      }));
+  }
 }
